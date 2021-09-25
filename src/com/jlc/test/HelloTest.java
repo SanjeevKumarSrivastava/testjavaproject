90ff01a94982ea7996c3b15f18a1c3ef612b6564
SanjeevKumarSrivastava <sksanjeev4455@gmail.com>
2021-09-25 23:47:12 +0530

    first commit

---
+++ src/com/jlc/test/HelloTest.java
@@ -0,0 +1,9 @@
+package com.jlc.test;
+
+public class HelloTest {
+	
+	public static void testmethod() {
+		System.out.println("HelloTest: testmethod()");
+	}
+
+}
---
+++ src/com/jlc/test/JavaTest.java
@@ -0,0 +1,9 @@
+package com.jlc.test;
+
+public class JavaTest {
+	
+	public static void main(String[] args) {
+		System.out.println("we are under JavaTest class");
+	}
+
+}
